db4613b6073cf1261c818aad1cc872f25254cd2f
Oz Mercer <ozmercer@gmail.com>
2018-04-11 15:36:15 +0300

    remove extras

--- js/routes.js
+++ js/routes.js
@@ -1,13 +1,9 @@
-import mail from "./pages/mail/mail.js";
-import map from "./pages/map/map.js";
-import note from "./pages/note/note.js";
 import home from "./pages/home.js";
+import mail from "./pages/mail/mail.js";
 
 const routes = [
     {path: '/', component: home},
     {path: '/mail', component: mail},
-    {path: '/map', component: map},
-    {path: '/note', component: note},
 ]
 
 Vue.use(VueRouter);
