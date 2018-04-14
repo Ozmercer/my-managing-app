90fe420f3cfbbffd74d3107c238ed957cbafcaa1
Oz Mercer <ozmercer@gmail.com>
2018-04-14 22:49:59 +0300

    merging

--- js/pages/mail/mail.js
+++ js/pages/mail/mail.js
@@ -59,7 +59,7 @@ export default {
                 </div>
                 <hr>
                 <div class="sorry">
-                    <p><strong>SORRY!<strong></p>
+                    <p><strong>SORRY!</strong></p>
                     <p>Due to technical difficulties, you can currently only recieve E-Mails 
                         from yourself
                     </p>
